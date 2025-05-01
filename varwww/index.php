db15ece5ea8987a45c2edff5a0d51aaeca689d66
root <root@by51.bve.lan>
2025-05-01 12:18:17 +0200

    adding icon menu in nav bar

--- varwww/index.php
+++ varwww/index.php
@@ -94,6 +94,9 @@ $containers = get_docker_data( 'http://localhost/v1.45/containers/json?all=1' );
             color: white;
         }
 
+        nav .title a:hover {
+            color: #CCC;
+        }
         nav .menu {
             display: none;
         }
@@ -111,6 +114,38 @@ $containers = get_docker_data( 'http://localhost/v1.45/containers/json?all=1' );
             color: white;
             text-decoration: none;
         }
+
+
+
+        nav ul.icons {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333333;
+        }
+
+        nav ul.icons li {
+            float: left;
+        }
+
+        nav ul.icons li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 16px;
+            text-decoration: none;
+        }
+
+        nav ul.icons li a:hover {
+            background-color: #111111;
+        }
+
+
+
+
+
+
 
         nav .search-container {
             display: flex;
@@ -197,6 +232,7 @@ $containers = get_docker_data( 'http://localhost/v1.45/containers/json?all=1' );
                 padding: 10px;
                 display: block;
             }
+
         }
 
         /* Main content */
@@ -260,9 +296,14 @@ $containers = get_docker_data( 'http://localhost/v1.45/containers/json?all=1' );
     <!-- Navigation Bar -->
     <nav>
         <div class="title"><a href="/">Docker Networks</a></div>
-        <div>
-            <a href="#ipam">IP</a>
-        </div>
+        <ul class="icons">
+            <li id="networks-icon"><a href="#networks" aria-label="Networks"><img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' version='1'%3E%3Cpath fill='%23FFFFFF' d='M10,2C8.89,2 8,2.89 8,4V7C8,8.11 8.89,9 10,9H11V11H2V13H6V15H5C3.89,15 3,15.89 3,17V20C3,21.11 3.89,22 5,22H9C10.11,22 11,21.11 11,20V17C11,15.89 10.11,15 9,15H8V13H16V15H15C13.89,15 13,15.89 13,17V20C13,21.11 13.89,22 15,22H19C20.11,22 21,21.11 21,20V17C21,15.89 20.11,15 19,15H18V13H22V11H13V9H14C15.11,9 16,8.11 16,7V4C16,2.89 15.11,2 14,2H10M10,4H14V7H10V4M5,17H9V20H5V17M15,17H19V20H15V17Z'%3E%3C/path%3E%3C/svg%3E" /></a></li>
+            <li id="ipam-icon"><a href="#ipam" aria-label="IP"><img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' version='1'%3E%3Cpath fill='%23FFFFFF' d='M15,9H13V7H15V9M22,20V22H15A1,1 0 0,1 14,23H10A1,1 0 0,1 9,22H2V20H9A1,1 0 0,1 10,19H11V17H7A2,2 0 0,1 5,15V5A2,2 0 0,1 7,3H17A2,2 0 0,1 19,5V15A2,2 0 0,1 17,17H13V19H14A1,1 0 0,1 15,20H22M9,5H7V15H9V5M11,15H13V11H15A2,2 0 0,0 17,9V7A2,2 0 0,0 15,5H11V15Z'%3E%3C/path%3E%3C/svg%3E" /></a></li>
+            <li id="ports-icon"><a href="#ports" aria-label="Ports"><img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' version='1'%3E%3Cpath fill='%23FFFFFF' d='M9 6V11H7V7H5V11H3V9H1V21H3V19H5V21H7V19H9V21H11V19H13V21H15V19H17V21H19V19H21V21H23V9H21V11H19V7H17V11H15V6H13V11H11V6H9M3 13H5V17H3V13M7 13H9V17H7V13M11 13H13V17H11V13M15 13H17V17H15V13M19 13H21V17H19V13Z'%3E%3C/path%3E%3C/svg%3E" /></a></li>
+            <li id="images-icon"><a href="#images" aria-label="Images"><img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' version='1'%3E%3Cpath fill='%23FFFFFF' d='M8.5,13.5L11,16.5L14.5,12L19,18H5M21,19V5C21,3.89 20.1,3 19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19Z'%3E%3C/path%3E%3C/svg%3E" /></a></li>
+            <li id="containers-icon"><a href="#containers" aria-label="Containers"><img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' version='1'%3E%3Cpath fill='%23FFFFFF' d='M21,16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V7.5C3,7.12 3.21,6.79 3.53,6.62L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.79,6.79 21,7.12 21,7.5V16.5M12,4.15L6.04,7.5L12,10.85L17.96,7.5L12,4.15M5,15.91L11,19.29V12.58L5,9.21V15.91M19,15.91V9.21L13,12.58V19.29L19,15.91Z'%3E%3C/path%3E%3C/svg%3E" /></a></li>
+
+        </ul>
         <div class="search-container">
             <button class="clear-btn" id="clearSearchBtn" style="display:none;">Clear</button>
             <input type="search" id="searchBar" placeholder="Search...">
@@ -308,9 +349,9 @@ $containers = get_docker_data( 'http://localhost/v1.45/containers/json?all=1' );
                     echo "<td>{$d['Driver']}</td>";
                     echo "<td>".($d['Labels']['com.docker.compose.project']?:"<i>Default</i>")."</td>";
                     echo "<td>{$d['Subnet']}</td>";
-                    echo "<td>".($d['Labels']['com.docker.compose.project']? $d['total_ips'] :"??")."</td>";
-                    echo "<td>".($d['used_ips']<=2? "<span style='color: red;' title='I hope you have future plans with this...'>".$d['used_ips']."</span>" :$d['used_ips'])."</td>"; 
-                    echo "<td>".($d['Labels']['com.docker.compose.project']? $d['available_ips'] :"??")."</td>";
+                    echo "<td style='text-align: right;'>".($d['Labels']['com.docker.compose.project']? $d['total_ips'] :"??")."</td>";
+                    echo "<td style='text-align: right;'>".($d['used_ips']<=2? "<span style='color: red;' title='I hope you have future plans with this...'>".$d['used_ips']."</span>" :$d['used_ips'])."</td>"; 
+                    echo "<td style='text-align: right;'>".($d['Labels']['com.docker.compose.project']? $d['available_ips'] :"??")."</td>";
                     echo "</tr>\n";
                 }
 ?>
@@ -407,10 +448,10 @@ $containers = get_docker_data( 'http://localhost/v1.45/containers/json?all=1' );
 			            if( $p['IP']=="::" ) continue;
 
                         print "                    <tr>";
-                        print "<td>{$p['PublicPort']}</td>";
+                        print "<td style='text-align: right;'>{$p['PublicPort']}</td>";
                         // print "<td>{$p['IP']}</td>";
                         print "<td>{$p['Type']}</td>";
-                        print "<td>{$p['PrivatePort']}</td>";
+                        print "<td style='text-align: right;'>{$p['PrivatePort']}</td>";
                         print "<td>".substr( $c['Names'][0], 1 )."</td>";
                         // print "<td>". ( str_contains($c['Image'], ":") ? substr( $c['Image'], 0, strpos($c['Image'], ":") ) : $c['Image'] )."</td>";
                         // print "<td>". ( str_contains($c['Image'], ":") ? substr( $c['Image'], 0, strpos($c['Image'], ":") ) : $c['Image'] )."</td>";
@@ -482,10 +523,10 @@ $containers = get_docker_data( 'http://localhost/v1.45/containers/json?all=1' );
                     print "<td>{$tag}</td>";
                     print "<td>".substr( $img['Id'], strpos($img['Id'], ":")+1, 12 )."</td>";
                 //    print "<td>". ( $img_used_running>0 ? $img_used_running : "<span style='color: red;font-weight: bold'>".$img_used_running."</span>" ) ."</td>";
-                    print "<td>". ( $img_used_running>0 ? ( $img_used_running==1 ? $img_used_running : "<span style='font-weight: bold'>".$img_used_running."</span>" ) : "<span style='color: red;'>".$img_used_running."</span>" ) ."</td>";
-                    print "<td>". ( $img_used>0 ? ( $img_used==1 ? $img_used : "<span style='font-weight: bold'>".$img_used."</span>" ) : "<span style='color: red;'>".$img_used."</span>" ) ."</td>";
+                    print "<td style='text-align: right;'>". ( $img_used_running>0 ? ( $img_used_running==1 ? $img_used_running : "<span style='font-weight: bold'>".$img_used_running."</span>" ) : "<span style='color: red;'>".$img_used_running."</span>" ) ."</td>";
+                    print "<td style='text-align: right;'>". ( $img_used>0 ? ( $img_used==1 ? $img_used : "<span style='font-weight: bold'>".$img_used."</span>" ) : "<span style='color: red;'>".$img_used."</span>" ) ."</td>";
                 //    print "<td>". ( $img_used>0 ? $img_used : "<span style='color: red;font-weight: bold'>".$img_used."</span>" ) ."</td>";
-                    print "<td>".str_pad( round($img['Size']/1024/1024 ,0), 8, ' ', STR_PAD_LEFT)."</td>";
+                    print "<td style='text-align: right;'>".str_pad( round($img['Size']/1024/1024 ,0), 8, ' ', STR_PAD_LEFT)."</td>";
                     print "<td><span title=\"".gmdate("Y-m-d\TH:i:s\Z", $img['Created'])."\">".gmdate("Y-m-d", $img['Created'])."</span></td>";
 			$labels_hide = array( 
 				 'com.docker.compose.image'
@@ -583,7 +624,7 @@ $containers = get_docker_data( 'http://localhost/v1.45/containers/json?all=1' );
 
 
 
-                        print "<td align='right'>".implode( " - ", array_keys($ports) )."</td>";
+                        print "<td style='text-align: right;'>".implode( " - ", array_keys($ports) )."</td>";
 
                         print "</tr>\n";
 		    }
@@ -656,6 +697,7 @@ $containers = get_docker_data( 'http://localhost/v1.45/containers/json?all=1' );
                 document.getElementById(sum_id).innerHTML = row_count;
                 document.getElementById(table_id).style.display = '';
                 document.getElementById(table_id.concat("-table")).style.display = '';
+                document.getElementById(table_id.concat("-icon")).style.display = '';
 
             });
         }
@@ -691,9 +733,11 @@ $containers = get_docker_data( 'http://localhost/v1.45/containers/json?all=1' );
                 if (row_count==0) {                                                                 // Hide empty blocks
                     document.getElementById(table_id).style.display = 'none';                       //  h1 title
                     document.getElementById(table_id.concat("-table")).style.display = 'none';      // table
+                    document.getElementById(table_id.concat("-icon")).style.display = 'none';      // table
                 } else {
                     document.getElementById(table_id).style.display = '';                           //  h1 title
                     document.getElementById(table_id.concat("-table")).style.display = '';          // table
+                    document.getElementById(table_id.concat("-icon")).style.display = '';          // table
 
                 }
 
@@ -749,7 +793,7 @@ $containers = get_docker_data( 'http://localhost/v1.45/containers/json?all=1' );
 
 
 
-<!-- details
+<!-- network details
     <div>
 	<pre>
 	<?php print_r( $details ); ?>
